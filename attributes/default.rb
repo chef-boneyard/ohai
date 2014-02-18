0b3e4ff2ed0cf4a76d0e47e556129ce19efb2515
Nolan Davidson <nolan.davidson@gmail.com>
2014-02-18 14:51:34 -0500

    Updated default attribute for Windows plugin path to use %systemdrive% instead of C:

--- attributes/default.rb
+++ attributes/default.rb
@@ -20,7 +20,7 @@
 # FHS location would be /var/lib/chef/ohai_plugins or similar.
 case node["platform_family"]
 when "windows"
-	default["ohai"]["plugin_path"] = "C:/chef/ohai_plugins"
+	default["ohai"]["plugin_path"] = "#{ENV['systemdrive']}/chef/ohai_plugins"
 else
 	default["ohai"]["plugin_path"] = "/etc/chef/ohai_plugins"
 end
