5416e53c0588cb5d161cf2120306b33da30742c2
Tim Smith <tsmith84@gmail.com>
2015-09-08 09:48:08 -0700

    Rubocop fixes

--- attributes/default.rb
+++ attributes/default.rb
@@ -18,14 +18,14 @@
 #
 
 # FHS location would be /var/lib/chef/ohai_plugins or similar.
-case node["platform_family"]
-when "windows"
-	default["ohai"]["plugin_path"] = "C:/chef/ohai_plugins"
-	default["ohai"]["hints_path"] = "C:/chef/ohai/hints"
+case node['platform_family']
+when 'windows'
+  default['ohai']['plugin_path'] = 'C:/chef/ohai_plugins'
+  default['ohai']['hints_path'] = 'C:/chef/ohai/hints'
 else
-	default["ohai"]["plugin_path"] = "/etc/chef/ohai_plugins"
-	default["ohai"]["hints_path"] = "/etc/chef/ohai/hints"
+  default['ohai']['plugin_path'] = '/etc/chef/ohai_plugins'
+  default['ohai']['hints_path'] = '/etc/chef/ohai/hints'
 end
 
 # The list of plugins and their respective file locations
-default["ohai"]["plugins"]["ohai"] = "plugins"
+default['ohai']['plugins']['ohai'] = 'plugins'
--- metadata.rb
+++ metadata.rb
@@ -1,30 +1,30 @@
-name             "ohai"
-maintainer       "Chef Software, Inc"
-maintainer_email "cookbooks@chef.io"
-license          "Apache 2.0"
-description      "Distributes a directory of custom ohai plugins"
+name 'ohai'
+maintainer 'Chef Software, Inc'
+maintainer_email 'cookbooks@chef.io'
+license 'Apache 2.0'
+description 'Distributes a directory of custom ohai plugins'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "2.0.2"
+version '2.0.2'
 
-recipe "ohai::default", "Distributes a directory of custom ohai plugins"
+recipe 'ohai::default', 'Distributes a directory of custom ohai plugins'
 
 source_url 'https://github.com/chef-cookbooks/ohai' if respond_to?(:source_url)
 issues_url 'https://github.com/chef-cookbooks/ohai/issues' if respond_to?(:issues_url)
 
 %w(ubuntu debian centos redhat amazon scientific fedora oracle).each do |os|
-    supports os
+  supports os
 end
 
-attribute "ohai/plugin_path",
-  :display_name => "Ohai Plugin Path",
-  :description => "Distribute plugins to this path.",
-  :type => "string",
-  :required => "optional",
-  :default => "/etc/chef/ohai_plugins"
+attribute 'ohai/plugin_path',
+  display_name: 'Ohai Plugin Path',
+  description: 'Distribute plugins to this path.',
+  type: 'string',
+  required: 'optional',
+  default: '/etc/chef/ohai_plugins'
 
-attribute "ohai/plugins",
-  :display_name => "Ohai Plugin Sources",
-  :description => "Read plugins from these cookbooks and paths",
-  :type => "hash",
-  :required => "optional",
-  :default => {'ohai' => 'plugins'}
+attribute 'ohai/plugins',
+  display_name: 'Ohai Plugin Sources',
+  description: 'Read plugins from these cookbooks and paths',
+  type: 'hash',
+  required: 'optional',
+  default: { 'ohai' => 'plugins' }
