22ea1c97a39442f076e6da7f2d6c94b0897e64de
Tim Smith <tsmith84@gmail.com>
2015-09-08 09:46:22 -0700

    Add platform support and supermarket metadata

--- metadata.rb
+++ metadata.rb
@@ -8,6 +8,13 @@ version          "2.0.2"
 
 recipe "ohai::default", "Distributes a directory of custom ohai plugins"
 
+source_url 'https://github.com/chef-cookbooks/ohai' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/ohai/issues' if respond_to?(:issues_url)
+
+%w(ubuntu debian centos redhat amazon scientific fedora oracle).each do |os|
+    supports os
+end
+
 attribute "ohai/plugin_path",
   :display_name => "Ohai Plugin Path",
   :description => "Distribute plugins to this path.",
