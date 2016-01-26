df1f9d823f3f6452fd2a29088bc8e8edde983282
Tim Smith <tsmith84@gmail.com>
2016-01-26 12:02:18 -0800

    Add the correct license to the test cookbook

--- test/cookbooks/ohai_test/metadata.rb
+++ test/cookbooks/ohai_test/metadata.rb
@@ -1,7 +1,7 @@
 name 'ohai_test'
 maintainer 'HiganWorks LLC.'
 maintainer_email 'sawanoboriyu@higanworks.com'
-license ''
+license 'Apache 2.0'
 description 'Installs/Configures ohai_test'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
