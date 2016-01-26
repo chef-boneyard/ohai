60681a4803848cc6e1efed6991c6d6806011b2c0
Tim Smith <tsmith84@gmail.com>
2016-01-26 13:20:53 -0800

    Converge the test recipe as well

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -11,3 +11,15 @@ describe 'default recipe on Ubuntu 14.04' do
     expect { :chef_run }.to_not raise_error
   end
 end
+
+describe 'test recipe on Ubuntu 14.04' do
+  let(:chef_run) do
+    ChefSpec::ServerRunner.new do |node|
+      node.automatic[:lsb][:codename] = 'trusty'
+    end.converge('ohai_test::default')
+  end
+
+  it 'converges successfully' do
+    expect { :chef_run }.to_not raise_error
+  end
+end
