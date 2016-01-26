3b6a98fa9027b0c7d06a6be1e49ee9a8eb559980
Tim Smith <tsmith84@gmail.com>
2016-01-26 14:10:31 -0800

    Add windows as a supported platform

--- metadata.rb
+++ metadata.rb
@@ -11,6 +11,6 @@ recipe 'ohai::default', 'Distributes a directory of custom ohai plugins'
 source_url 'https://github.com/chef-cookbooks/ohai' if respond_to?(:source_url)
 issues_url 'https://github.com/chef-cookbooks/ohai/issues' if respond_to?(:issues_url)
 
-%w(ubuntu debian centos redhat amazon scientific fedora oracle freebsd).each do |os|
+%w(ubuntu debian centos redhat amazon scientific fedora oracle freebsd windows).each do |os|
   supports os
 end
