3cd88337fc353fa7521d8928f9ac0f4f09ee5eff
Tim Smith <tsmith84@gmail.com>
2016-03-09 09:41:40 -0800

    Reorder metadata

--- metadata.rb
+++ metadata.rb
@@ -8,9 +8,10 @@ version '2.1.0'
 
 recipe 'ohai::default', 'Distributes a directory of custom ohai plugins'
 
-source_url 'https://github.com/chef-cookbooks/ohai' if respond_to?(:source_url)
-issues_url 'https://github.com/chef-cookbooks/ohai/issues' if respond_to?(:issues_url)
-
 %w(ubuntu debian centos redhat amazon scientific fedora oracle freebsd windows).each do |os|
   supports os
 end
+
+source_url 'https://github.com/chef-cookbooks/ohai' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/ohai/issues' if respond_to?(:issues_url)
+
