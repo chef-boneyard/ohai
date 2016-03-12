81652dc8d4436ffaaeaef6967307a1a37997c1ce
Tim Smith <tsmith84@gmail.com>
2016-03-11 20:38:54 -0800

    Remove legacy recipe functionality
    We shouldn't encourage users to fork a cookbook and drop their own files into the files directories.  This is why we have custom resources and wrapper cookbooks now.

--- metadata.rb
+++ metadata.rb
@@ -2,11 +2,9 @@ name 'ohai'
 maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
-description 'Distributes a directory of custom ohai plugins'
+description 'Provides custom resources for installing Ohai hints and plugins'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.1.0'
-
-recipe 'ohai::default', 'Distributes a directory of custom ohai plugins'
 
 %w(ubuntu debian centos redhat amazon scientific fedora oracle freebsd windows).each do |os|
   supports os
@@ -14,4 +12,3 @@ end
 
 source_url 'https://github.com/chef-cookbooks/ohai' if respond_to?(:source_url)
 issues_url 'https://github.com/chef-cookbooks/ohai/issues' if respond_to?(:issues_url)
-
