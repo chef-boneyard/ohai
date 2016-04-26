62467f6dffbfc35c287aa54e3c4361c1fe14ba3f
Tim Smith <tsmith84@gmail.com>
2016-04-26 14:28:41 -0700

    Convert hint over to an actual 12.5+ resource

--- resources/hint.rb
+++ resources/hint.rb
@@ -1,23 +1,23 @@
-actions :create, :delete
-
 property :hint_name, kind_of: String, name_attribute: true
 property :content, kind_of: Hash
 
-def ohai_hint_path
-  ::File.join(::Ohai::Config[:hints_path].first, name)
-end
+action_class.class_eval do
+  def ohai_hint_path
+    ::File.join(::Ohai::Config[:hints_path].first, name)
+  end
 
-def build_content
-  # passing nil to file produces deprecation warnings so pass an empty string
-  return nil if content.nil? || content.empty?
-  JSON.pretty_generate(content)
-end
+  def build_content
+    # passing nil to file produces deprecation warnings so pass an empty string
+    return nil if content.nil? || content.empty?
+    JSON.pretty_generate(content)
+  end
 
-def file_content(path)
-  return JSON.parse(::File.read(path))
-rescue JSON::ParserError
-  Chef::Log.debug("Could not parse JSON in ohai hint at #{ohai_hint_path}. It's probably an empty hint file")
-  return nil
+  def file_content(path)
+    return JSON.parse(::File.read(path))
+  rescue JSON::ParserError
+    Chef::Log.debug("Could not parse JSON in ohai hint at #{ohai_hint_path}. It's probably an empty hint file")
+    return nil
+  end
 end
 
 load_current_value do
