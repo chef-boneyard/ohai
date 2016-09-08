4c6e1974783c5637223beb4f5edbd1b9b4ad2f71
Tim Smith <tsmith@chef.io>
2016-09-08 12:44:36 -0700

    Require Chef 12.1 not 12.0
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -15,4 +15,4 @@ depends 'compat_resource', '>= 12.14'
 source_url 'https://github.com/chef-cookbooks/ohai'
 issues_url 'https://github.com/chef-cookbooks/ohai/issues'
 
-chef_version '>= 12.0'
+chef_version '>= 12.1'
