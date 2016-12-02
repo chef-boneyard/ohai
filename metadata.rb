f6b1532da2f12bc20589c8a4f04ca821a0aa2b24
Tim Smith <tsmith@chef.io>
2016-12-02 14:40:31 -0800

    Add suse as a supported platform
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description 'Provides custom resources for installing Ohai hints and plugins'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '4.2.2'
 
-%w(ubuntu debian centos redhat amazon scientific fedora oracle opensuse opensuseleap freebsd windows zlinux).each do |os|
+%w(ubuntu debian centos redhat amazon scientific fedora oracle suse opensuse opensuseleap freebsd windows zlinux).each do |os|
   supports os
 end
 
