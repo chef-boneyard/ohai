017a9733accedd89f436392267818396628f2ec7
Tim Smith <tsmith@chef.io>
2017-01-08 14:53:33 -0800

    Require the latest compat_resource
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -10,7 +10,7 @@ version '4.2.3'
   supports os
 end
 
-depends 'compat_resource', '>= 12.14.7'
+depends 'compat_resource', '>= 12.16.3'
 
 source_url 'https://github.com/chef-cookbooks/ohai'
 issues_url 'https://github.com/chef-cookbooks/ohai/issues'
